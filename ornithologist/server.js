90818fe0c6b32b0fc17c1b66828db080a6e38960
Matthew Norman <mnorman@Workshop.workshop>
2014-03-23 18:52:16 -0700

    Changed to using Express

--- ornithologist/server.js
+++ ornithologist/server.js
@@ -1,6 +1,15 @@
 var http = require('http');
-http.createServer(function (req, res) {
-    console.log('I am creating a server')
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('I am writing out some text')
-}).listen(8888, '127.0.0.1');
+var express = require('express');
+var app = express();
+
+app.configure(function() {
+    app.set('port', 8888);
+});
+
+app.get('/', function(req, res) {
+    res.send('I am writing out some text');
+});
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Beginning to listen: %d', app.get('port'))
+});
